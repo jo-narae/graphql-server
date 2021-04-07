d65d86794bd2217b78b9cd2ffd5df82d25124089
jo-narae <nare6004@gmail.com>
2021-04-08 07:46:40 +0900

    아티스트 CUD API 추가

--- src/db/repository/artist.js
+++ src/db/repository/artist.js
@@ -4,7 +4,22 @@ async function findAll(args) {
   return await models.Artist.findAll(args);
 }
 
+async function create(args) {
+  return await models.Artist.create(args);
+}
+
+async function update(args) {
+  return await models.Artist.update(args, { where: { id: args.id } });
+}
+
+async function destroy(args) {
+  return await models.Artist.destroy({ where: { id: args.id } });
+}
+
 module.exports = {
-  findAll
+  findAll,
+  create,
+  update,
+  destroy
 };
 
--- src/router/artist.js
+++ src/router/artist.js
@@ -4,8 +4,77 @@ const router = new Router();
 const models = require('../db/models');
 const repository = require('../db/repository');
 
+const Joi = require('joi');
+
 router.get('/', async (ctx) => {
   ctx.body = await repository.Artist.findAll({ include: models.Agency });
+});
+
+router.get('/:id', async (ctx) => {
+  ctx.body = await repository.Artist.findAll({ where: { id: ctx.params.id }, include: models.Agency });
+});
+
+router.post('/', async (ctx) => {
+
+  const req = ctx.request.body;
+
+  const schema = Joi.object({
+    name: Joi.string().min(1).max(50).required(),
+    debut_date: Joi.string().min(1).max(10).required(),
+    agency_id: Joi.number().required()
+  });
+
+  try {
+    await schema.validateAsync(req);
+    req.agency = await repository.Agency.findAll({ where: { id: req.agency_id } });
+    ctx.body = await repository.Artist.create(req);
+  }
+  catch (err) {
+    ctx.status = 400; // bad request
+    ctx.body = err;
+  }
+});
+
+router.put('/:id', async (ctx) => {
+
+  const req = ctx.request.body;
+
+  const schema = Joi.object({
+    id: Joi.number().required(),
+    name: Joi.string().min(1).max(50).required(),
+    debut_date: Joi.string().min(1).max(10).required(),
+    agency_id: Joi.number().required()
+  });
+
+  req.id = ctx.params.id;
+
+  try {
+    await schema.validateAsync(req);
+    req.agency = await repository.Agency.findAll({ where: { id: req.agency_id } });
+    ctx.body = await repository.Artist.update(req);
+  }
+  catch (err) {
+    ctx.status = 400; // bad request
+    ctx.body = err;
+  }
+});
+
+router.delete('/:id', async (ctx) => {
+
+  const req = { id: ctx.params.id };
+
+  const schema = Joi.object({
+    id: Joi.number().required()
+  });
+
+  try {
+    await schema.validateAsync(req);
+    ctx.body = await repository.Artist.destroy(req);
+  }
+  catch (err) {
+    ctx.status = 400; // bad request
+    ctx.body = err;
+  }
 });
 
 module.exports = router;
