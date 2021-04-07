0e4975b9687989496a5c3cd6af60174b3a1c6ea6
jo-narae <nare6004@gmail.com>
2021-04-08 01:17:10 +0900

    불필요 코드 삭제

--- src/db/repository/agency.js
+++ src/db/repository/agency.js
@@ -1,11 +1,7 @@
 const models = require('../models');
 
 async function findAll(args) {
-  let results = null;
-  await models.Agency.findAll(args)
-    .then(res => results = res)
-    .catch(err => results = err);
-  return results;
+  return await models.Agency.findAll(args);
 }
 
 module.exports = {
--- src/db/repository/artist.js
+++ src/db/repository/artist.js
@@ -1,11 +1,7 @@
 const models = require('../models');
 
 async function findAll(args) {
-  let results = null;
-  await models.Artist.findAll(args)
-    .then(res => results = res)
-    .catch(err => results = err);
-  return results;
+  return await models.Artist.findAll(args);
 }
 
 module.exports = {
